216eb967f47a4d3feaca701717ffd1b60dd33768
cukinhou <j.nistalhurle@gmail.com>
2017-05-15 17:13:21 +0200

    Adds signals.py

--- measurement/signals.py
+++ measurement/signals.py
@@ -7,17 +7,19 @@ from scipy.fftpack import fft, ifft
 from scipy.signal import chirp
 from abc import ABCMeta, abstractmethod
 
+
 class ExcitationSignal:
     __metaclass__ = ABCMeta
-    def __init__(self, type='sweep', sample_rate=44100, duration=5):
-        self.type = type
+
+    def __init__(self, signal_type='sweep', sample_rate=44100, duration=5):
+        self.type = signal_type
         self.sample_rate = sample_rate
         self.duration = duration
-        
+
     @abstractmethod
     def create(self):
         pass
-    
+
     @abstractmethod
     def get_ir(self):
         pass
@@ -25,33 +27,30 @@ class ExcitationSignal:
 
 class SineSweep(ExcitationSignal):
     def __init__(
-            self, 
-            type='sweep', 
-            sample_rate=44100, 
-            duration=5, 
-            method='logarithmic'
-        ):
-        
+        self,
+        type='sweep',
+        sample_rate=44100,
+        duration=5,
+        method='logarithmic'
+    ):
         super(SineSweep, self).__init__(type, sample_rate, duration)
         self.method = method
-        
+
     def create(self):
-        
+
         t = np.linspace(
-                0, 
-                self.duration, 
-                self.sample_rate * self.duration - 1
-            )
+            0,
+            self.duration,
+            self.sample_rate * self.duration - 1
+        )
         sweep = chirp(t, 20, self.duration, 20000, self.method) * 32767
-        
+
         return sweep.astype(np.int16)
 
     @staticmethod
     def get_ir(sweep, sweep_response):
-
         y = fft(sweep_response, len(sweep_response))
         x = fft(sweep, len(sweep_response))
+        ir = ifft(np.divide(y, x)).real
 
-        ir = ifft(np.divide(y,x)).real
-
-        return np.around([x*32768 for x in ir])
+        return np.around([item*32768 for item in ir])
